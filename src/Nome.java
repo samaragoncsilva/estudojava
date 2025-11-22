473d8cc32c58055ca106f67d4a87095ee0fdf64f
samaragoncsilva <samaarasilva222220@gmail.com>
2025-11-22 18:09:56 -0300

    adicionando codigo nome

---
+++ src/Nome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Nome {
+    public static void main (String[] args){
+        String x,y;
+        y="luna";
+        x="maria";
+
+        System.out.printf("%s, %s\n", x,y);
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
