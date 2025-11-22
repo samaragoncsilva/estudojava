648adcb6517d3a8a063fe1318d4f9504fa89910d
samaragoncsilva <samaarasilva222220@gmail.com>
2025-11-22 16:56:45 -0300

    Adicionando Pessoa.java

---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Pessoa {
+    public static void main(String[] args){
+        int idade= 20;
+        String nome= "samara";
+        double salario = 1.100;
+
+        System.out.printf("%s tem %d anos e ganha r$: %.3f reais%n", nome, idade, salario);
+    }
+}
